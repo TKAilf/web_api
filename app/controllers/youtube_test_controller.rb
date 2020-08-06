e587d1ef996dbf9d4e85d477dd573cd546031ad2
ktoshiki0511 <ktoshiki0511@yahoo.co.jp>
2020-08-07 08:29:21 +0900

    youtube_testファイル追加

---
+++ app/controllers/youtube_test_controller.rb
@@ -0,0 +1,39 @@
+require 'google/apis/youtube_v3'
+
+class YoutubeTestController < ApplicationController
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = ENV['ENV_API_KEY']
+
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: 'video',
+      max_results: 3,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601
+    }
+    service.list_searches(:snippet, opt)
+  end
+
+  def find_video_categories
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = ENV['ENV_API_KEY']
+
+    opt = {
+      chart: "mostPopular",
+      hl: "ja",
+      max_results: 5,
+      region_code: "jp",
+      video_category_id: "20"
+    }
+    service.list_videos(:snippet, opt)
+  end
+
+  def index
+    @youtube_data = find_video_categories
+  end
+end
---
+++ app/helpers/youtube_test_helper.rb
@@ -0,0 +1,2 @@
+module YoutubeTestHelper
+end
