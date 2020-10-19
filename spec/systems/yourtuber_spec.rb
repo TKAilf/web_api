5fe89e1a939756c7e50213c317618f69c86fc6c0
ktoshiki0511 <ktoshiki0511@yahoo.co.jp>
2020-10-19 09:55:00 +0900

    navigation_bar更新 & JS:auto_reload追加

--- spec/systems/yourtuber_spec.rb
+++ spec/systems/yourtuber_spec.rb
@@ -12,5 +12,22 @@ RSpec.describe "Yourtubers", type: :system do
     before do
       visit yourtuber_index_path
     end
+
+    describe "navigation.html.erb" do
+      it "ボタンが3つ存在する" do
+        within "#site-navigation" do
+          expect(page).to have_link "タグについて"
+          expect(page).to have_link "yourtuberでタグ検索してみましょう！"
+          expect(page).to have_link "ホームに戻る"
+        end
+      end
+
+      it "JS:検証 ボタンをクリックする", js: true do
+        within "#site-navigation" do
+          click_on "タグについて"
+          expect(page).to have_link "javascript:cmanLinkScroll('post-1774')"
+        end
+      end
+    end
   end
 end
