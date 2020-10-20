67a7d476c8da9cb7e056ccb67d5803a4984b1dc2
ktoshiki0511 <ktoshiki0511@yahoo.co.jp>
2020-10-21 01:27:22 +0900

    index_navigationとindex_flexsliderのspec追加

--- spec/systems/yourtuber_spec.rb
+++ spec/systems/yourtuber_spec.rb
@@ -14,18 +14,34 @@ RSpec.describe "Yourtubers", type: :system do
     end
 
     describe "navigation.html.erb" do
-      it "ボタンが3つ存在する" do
-        within "#site-navigation" do
-          expect(page).to have_link "タグについて"
-          expect(page).to have_link "yourtuberでタグ検索してみましょう！"
-          expect(page).to have_link "ホームに戻る"
+      it "ページ内リンク:Youtubeタグについて に遷移する" do
+        within "#menu-item-1636" do
+          expect(page).to have_link "タグについて", href: "javascript:cmanLinkScroll('post-1774')"
         end
       end
+      it "ページ内リンク:Youtubeのタグ検索 に遷移する" do
+        within "#menu-item-2094" do
+          expect(page).to have_link "yourtuberでタグ検索してみましょう！", href: "javascript:cmanLinkScroll('post-1822')"
+        end
+      end
+      it "ページ内リンク:ページトップ に遷移する" do
+        within "#menu-item-1881" do
+          expect(page).to have_link "ホームに戻る", href: "javascript:cmanLinkScroll('page')"
+        end
+      end
+    end
 
-      it "JS:検証 ボタンをクリックする", js: true do
-        within "#site-navigation" do
-          click_on "タグについて"
-          expect(page).to have_link "javascript:cmanLinkScroll('post-1774')"
+    describe "flex_slider.html.erb" do
+      it "ページ内リンク:Youtubeタグについて に遷移する" do
+        within "#flexslider-tag-describe" do
+          expect(page).to have_link nil, href: "javascript:cmanLinkScroll('post-1774')"
+          expect(page).to have_link "ジャンプする", href: "javascript:cmanLinkScroll('post-1774')"
+        end
+      end
+      it "ページ内リンク:Youtubeのタグ検索 に遷移する" do
+        within "#flexslider-tag-search" do
+          expect(page).to have_link nil, href: "javascript:cmanLinkScroll('post-1822')"
+          expect(page).to have_link "ジャンプする", href: "javascript:cmanLinkScroll('post-1822')"
         end
       end
     end
