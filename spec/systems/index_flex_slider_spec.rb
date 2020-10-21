b4563ff5042ebe1785c5ef3d57016893befa5230
ktoshiki0511 <ktoshiki0511@yahoo.co.jp>
2020-10-21 09:15:00 +0900

    flexslider_spec追加

---
+++ spec/systems/index_flex_slider_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "index_navigation", type: :system do
+  describe "yourtuber#index" do
+    before do
+      visit yourtuber_index_path
+    end
+    describe "flex_slider.html.erb" do
+      it "ページ内リンク:Youtubeタグについて に遷移する" do
+        within "#flexslider-tag-describe" do
+          expect(page).to have_link nil, href: "javascript:cmanLinkScroll('post-1774')"
+          expect(page).to have_link "ジャンプする", href: "javascript:cmanLinkScroll('post-1774')"
+        end
+      end
+      it "ページ内リンク:Youtubeのタグ検索 に遷移する" do
+        within "#flexslider-tag-search" do
+          expect(page).to have_link nil, href: "javascript:cmanLinkScroll('post-1822')"
+          expect(page).to have_link "ジャンプする", href: "javascript:cmanLinkScroll('post-1822')"
+        end
+      end
+    end
+  end
+end
