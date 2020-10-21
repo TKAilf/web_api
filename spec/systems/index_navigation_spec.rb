bc9b95053e8be7fb068dd7740519dc591886b846
ktoshiki0511 <ktoshiki0511@yahoo.co.jp>
2020-10-21 09:15:18 +0900

    navigation_spec追加

---
+++ spec/systems/index_navigation_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "index_navigation", type: :system do
+  describe "yourtuber#index" do
+    before do
+      visit yourtuber_index_path
+    end
+
+    describe "navigation.html.erb" do
+      it "ページ内リンク:Youtubeタグについて に遷移する" do
+        within "#menu-item-1636" do
+          expect(page).to have_link "タグについて", href: "javascript:cmanLinkScroll('post-1774')"
+        end
+      end
+      it "ページ内リンク:Youtubeのタグ検索 に遷移する" do
+        within "#menu-item-2094" do
+          expect(page).to have_link "yourtuberでタグ検索してみましょう！", href: "javascript:cmanLinkScroll('post-1822')"
+        end
+      end
+      it "ページ内リンク:ページトップ に遷移する" do
+        within "#menu-item-1881" do
+          expect(page).to have_link "ホームに戻る", href: "javascript:cmanLinkScroll('page')"
+        end
+      end
+    end
+  end
+end
